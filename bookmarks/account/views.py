4b7e9764c1245b29682f718285a85ea42fa9ec73
mletunenko <mletunenko@gmail.com>
2024-03-11 11:25:39 +0300

    User registration added

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -9,9 +9,7 @@ def user_login(request):
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
+            user = authenticate(request, username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -36,12 +34,10 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Создать новый объект пользователя,
-            # но пока не сохранять его
+            # Создать новый объект пользователя, но пока не сохранять его
             new_user = user_form.save(commit=False)
             # Установить выбранный пароль
-            new_user.set_password(
-            user_form.cleaned_data['password'])
+            new_user.set_password(user_form.cleaned_data['password'])
             # Сохранить объект User
             new_user.save()
             return render(request, 'account/register_done.html', {'new_user': new_user})
